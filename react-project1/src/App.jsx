87afc65fbbe78d3d64c14cb953803367cddf651f
Dhruv Kachwala <133868125+Dhruv226-student@users.noreply.github.com>
2024-08-30 12:25:57 +0000

    todo

--- react-project1/src/App.jsx
+++ react-project1/src/App.jsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-   <h1>Hello </h1>
+      <center className="content">
+        <h1>TO do app</h1>
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <input type="text" placeholder="Enter Todo here" />
+            </div>
+            <div className="col-4">
+             <input type="date" />
+            </div>
+            <div className="col-2">
+              <button className="btn btn-success"></button>
+            </div>
+          </div>
+        </div>
+      </center>
+
     </>
   )
 }
